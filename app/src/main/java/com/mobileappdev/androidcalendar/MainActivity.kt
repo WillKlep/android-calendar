5201fc3863c24229726f145e1ec2318be6f4eb26
WillKlep <william.klepinger@ymail.com>
2023-03-31 09:47:34 -0400

    starting from scratch

--- app/src/main/java/com/mobileappdev/androidcalendar/MainActivity.kt
+++ app/src/main/java/com/mobileappdev/androidcalendar/MainActivity.kt
@@ -2,19 +2,53 @@ package com.mobileappdev.androidcalendar
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.GridView
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val monthSpinner = findViewById<Spinner>(R.id.spinner_month)
+        val yearSpinner = findViewById<Spinner>(R.id.spinner_year)
+
+        val monthsArray = arrayOf("January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December")
+
+        val arrayStartYear = 2030
+        val arrayEndYear = 1970
+        val yearsArray = Array(arrayStartYear - arrayEndYear + 1) { i -> arrayStartYear - i }
+
+        val monthArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, monthsArray)
+        monthArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        monthSpinner.adapter = monthArrayAdapter
+
+
+        monthSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var monthInt = parent?.getItemAtPosition(position);
+                Log.d("monthSpinner_output", monthInt.toString());
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+
+
+        val yearArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, yearsArray)
+        yearSpinner.adapter = yearArrayAdapter
 
         fun dayOfWeek(month: Int, day: Int, year: Int): Int {
             var month = month
@@ -31,6 +65,9 @@ class MainActivity : AppCompatActivity() {
             return h
         }
 
+    }
 
+    private fun getSpinnerPosition(spinner: Spinner) : Int{
+        return spinner.selectedItemPosition
     }
 }
